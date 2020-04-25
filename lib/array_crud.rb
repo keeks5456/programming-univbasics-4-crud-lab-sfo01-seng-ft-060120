8917654a71ac473e62dfbe3e0e2df6d061594198
Akiko Green <akikogreen08@gmail.com>
2020-04-25 00:32:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,8 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-    array = ["john", "jill", "jane", "jaime"]
-    array.pop(element)
+    array.unshift(element)
     p array
 end
 
